c4825271e7bfed2e5209482697047453179878c8
George Lugo <stg77713@gmail.com>
2020-03-11 02:50:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5;
   }
   do{
-    console.log("I run once regardless.")
+   array.pop();
   } while (array.length > 0 && maybeTrue());
     return array;
 }
