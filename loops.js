33dc0e8acbd050abe0a2d8ffb7099998f0da4625
George Lugo <stg77713@gmail.com>
2020-03-11 02:41:44 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,8 +4,22 @@ function forLoop(array){
        array.push("I am 1 strange loop.")
     }
     else{
-     array.push(`"I am ${i} strange loops."`)
+     array.push(`I am ${i} strange loops.`)
     }
   }
   return array;
+}
+
+function whileLoop(n){
+  let countdown = n;
+  while (countdown > 0){
+    console.log(--countdown);
+  }
+  return "done";
+}
+
+function doWhileLoop(num) {
+  do{
+    console.log("I run once regardless.");
+  } while (num === 0)
 }
